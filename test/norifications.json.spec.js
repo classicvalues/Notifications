a96200c8a003feaf2c51a6b369a75c5138581e6f
Artem <artem.horuzhenko@softeq.com>
2022-06-23 11:50:12 +0300

    add duplicaiton tests

--- test/norifications.json.spec.js
+++ test/norifications.json.spec.js
@@ -9,8 +9,19 @@ const schema = Joi.object({
 }).strict(true).required();
 
 describe('notification.json', () => {
+    const json = require('./../notifications.json');
+
     it('json should match schema', async () => {
-        const json = require('./../notifications.json');
         await schema.validateAsync(json);
+    });
+    it('check timestamp uniqueness', () => {
+        const timestamps = {};
+        json.notifications.length.forEach(({ timestamp }) => {
+            if (timestamps[timestamp]) {
+                return fail(`Timestamp: ${timestamp} has duplication!`);
+            } else {
+                timestamps[timestamp] = true;
+            }
+        });
     });
 });
